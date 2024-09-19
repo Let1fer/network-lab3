c5eb2942236fe4502843cb29d017a76d5b96caf9
let1fer <alekus2004@gmail.com>
2024-09-19 17:43:10 +0300

    fix

--- client/do.sh
+++ client/do.sh
@@ -1,5 +1,3 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
-echo $(ip -4 addr show eth0 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -n 1):12346
-echo $(ip -6 addr show eth0 | grep -oE '([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}' | head -n 1):12346
 sysctl -p
 nc -u -l -p 12346
--- server/do.sh
+++ server/do.sh
@@ -1,9 +1,13 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sysctl -p
-echo $(ip -4 addr show eth0 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -n 1):12345
-echo $(ip -6 addr show eth0 | grep -oE '([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}' | head -n 1):12345
-iptables -t nat -A PREROUTING -d 172.21.0.4 -p udp --dport 12345 -j DNAT --to $(ip -4 addr show eth0 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -n 1)
-ip6tables -t nat -A PREROUTING -d 2001:db8::4 -p udp --dport 12345 -j DNAT --to $(ip -6 addr show eth0 | grep -oE '([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}' | head -n 1)
+ip4=$(ip -4 addr show eth0 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -n 1)
+if [[ "$ip4" == "172.21.0.3" ]]; then 
+    ip6="2001:db8::3"
+else 
+    ip6="2001:db8::2"
+fi
+iptables -t nat -A PREROUTING -d 172.21.0.4 -p udp --dport 12345 -j DNAT --to $ip4
+ip6tables -t nat -A PREROUTING -d 2001:db8::4/64 -p udp --dport 12345 -j DNAT --to $ip6
 # iptables -t nat -A PREROUTING -d 172.21.0.4 -p tcp --dport 12345 -j DNAT --to $(ip -4 addr show eth0 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -n 1):12345
 # iptables -t filter -A OUTPUT -s 172.21.0.3 -p tcp -j DROP
 # iptables -t nat -A OUTPUT -p tcp -j DNAT --to 172.21.0.4:12346
